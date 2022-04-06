5b6d699ae32fcb6d7ad92afb2c75eebe7bafc958
Syed Huzaifa <shahhuzaifa338@gmail.com>
2022-04-06 20:26:53 +0500

    initialized reppo

---
+++ app/javascript/react/src/components/Hello.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react'                          
+import * as ReactDOM from 'react-dom'                   
+                                                        
+const Hello = () => {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+}                                                       
+                                        
+// Use it if you don't plan to use "remount"                
+// document.addEventListener('DOMContentLoaded', () => {     
+  // ReactDOM.render(<Hello />, document.getElementById('hello'))                  
+// })                                                    
+                                                        
+export default Hello
---
+++ app/javascript/react/src/index.js
@@ -0,0 +1,4 @@
+import { define } from "remount";
+import Hello from "./components/Hello.jsx";
+
+define({ "hello-component": Hello });
